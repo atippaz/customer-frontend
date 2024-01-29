f149bf858f9e99bc998aca5b1e691db57f45dbbe
atippaz <nasakun13201@gmail.com>
2024-01-29 18:09:12 +0700

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <HelloWorld />
+</template>
+
+<script setup>
+import HelloWorld from "@/components/HelloWorld.vue";
+(() => {
+  fetch("http://localhost:5255/")
+    .then((e) => e.json())
+    .then((e) => console.log(e))
+    .catch((e) => console.log(e));
+})();
+</script>
